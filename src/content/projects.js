40e83b0f4e184e584e542f6f26487ab1165893fb
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-08 12:35:09 -0700

    Rearranged project order

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,19 +1,5 @@
 const projectData = [
     {
-        id: "research-blog-project",
-        name: "Research Blog",
-        codeRepo: "",
-        site: "https://lyteway.com",
-        tech: [
-            "HTML", "CSS", "Python", "Pelican CMS"
-        ],
-        description: (
-            "This project is a personal blog to document my investment research." +
-            "It makes use of a flat file CMS called Pelican, that is built on Python."
-        ),
-    },
-
-    {
         id: "algoviz-project",
         name: "AlgoViz",
         codeRepo: "https://github.com/williamross165/AlgoViz",
@@ -25,7 +11,20 @@ const projectData = [
                      "animates popular algorithms. Check out more in the Github Repo below. " +
                      "This project is exlusively frontend and uses only " + 
                      "vanilla HTML, CSS, and Javascript.",
-    }
+    },
+    {
+        id: "research-blog-project",
+        name: "Research Blog",
+        codeRepo: "",
+        site: "https://lyteway.com",
+        tech: [
+            "HTML", "CSS", "Python", "Pelican CMS"
+        ],
+        description: (
+            "This project is a personal blog to document my investment research. " +
+            "It makes use of a flat file CMS called Pelican, that is built on Python."
+        ),
+    },
 ]
 
 export default projectData
