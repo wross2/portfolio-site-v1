6a93cc9d6092d882d5b451c1f4c1e8189622e179
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-09 19:37:01 -0700

    Created basic structure

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -5,19 +5,30 @@ const ProjectPanel = props => {
     const project = props.project
     return(
         <div className="project-card" id={project.id}>
-            {project.name}
+            <p>{project.name}</p>
+            <p>Description: {project.description}</p>
+            <span>
+                <a href={project.site} target="_blank" rel="noreferrer">Site </a>
+                <a href={project.codeRepo} target="_blank" rel="noreferrer">Code</a>
+            </span>
+            <ul>{
+                project.tech.map((tech, index) => {
+                    return <li key={index}>{tech}</li>
+                })
+            }</ul>
         </div>
     )
 }
 
 class Projects extends Component {
     render() {
+        const className = this.props.className;
         return (
-            <div className="section" id="section-projects">
+            <section className={className} id="projects-section">
                 {projectData.map(project => {
                     return <ProjectPanel key={project.id} project={project}/>
                 })}
-            </div>
+            </section>
         )
     }
 }
