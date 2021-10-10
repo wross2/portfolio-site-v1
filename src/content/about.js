31ba375010a2b05bcc0045f7726909bf7b86bfbd
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-09 18:16:54 -0700

    Added about section content/structure

---
+++ src/content/about.js
@@ -0,0 +1,24 @@
+const professionalInfo = (
+"I am a software developer who holds a B.Sc in Electrical Engineering. " +
+"I have worked on projects ranging from academic research on machine learning algorithms, " +
+"to government enterprise work, to school pojects in robotics. My soft skills include: " +
+"effective communicator, self-motivated learner, and I have a relentless drive to see things " +
+"through to completion."
+)
+
+const myTech = ["Python/Django", "Javascript/React", "HTML/CSS"]
+
+const myInterests = ""
+
+const aboutData = {
+    name: "William Ross",
+    professional: professionalInfo,
+    tech: myTech,
+    interests: myInterests,
+}
+
+
+
+
+
+export default aboutData
--- src/sections/About.js
+++ src/sections/About.js
@@ -1,8 +1,27 @@
 import React, {Component} from 'react'
+import aboutData from '../content/about'
 
 class About extends Component {
     render() {
-        return <div></div>
+        const className = this.props.className;
+        return (
+            <section className={className} id="about-section">
+                <div id="about-greeting">
+                    Hello my name is {aboutData.name}.
+                </div>
+                <div id="about-professional">
+                    {aboutData.professional}
+                </div>
+                <div id="about-tech">
+                    My web development tech stack includes:
+                    <ul id="tech-list">{
+                        aboutData.tech.map((tech, index) => {
+                            return <li key={index}>{tech}</li>
+                        })
+                    }</ul>
+                </div>
+            </section>
+        )
     }
 }
 
