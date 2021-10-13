9682b4b67490ffdc1217ca9e0b1a0669996b5efb
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-12 22:27:35 -0700

    Added typing effect to greeting

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react'
+import './styles/about.css'
 import aboutData from '../content/about'
+import TypeWriterEffect from 'react-typewriter-effect'
+
 
 class About extends Component {
     render() {
         const className = this.props.className;
+        const greeting = `Hello, my name is ${aboutData.name}`
+        const greetingFontSize = window.innerWidth < 500 ? '1.4rem' : '3.0rem';
+        console.log(window.innerWidth)
         return (
             <section className={className} id="about-section">
                 <div id="about-greeting">
-                    Hello my name is {aboutData.name}.
+                    <TypeWriterEffect
+                        textStyle={{
+                            color: 'var(--BLACK)',
+                            fontSize: greetingFontSize,
+                        }}
+                        startDelay={500}
+                        cursorColor="var(--PURPLE)"
+                        text={greeting}
+                        typeSpeed={100}
+                    />
                 </div>
                 <div id="about-professional">
                     {aboutData.professional}
