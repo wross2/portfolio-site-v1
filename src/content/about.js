6020cf834d07a5fb735fe752f479015aed2f6548
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-20 02:06:27 -0700

    Small content changes for aesthetics

--- src/content/about.js
+++ src/content/about.js
@@ -3,10 +3,10 @@ const professionalInfo = (
 "I have worked on a range of projects including research on machine learning algorithms, " +
 "government enterprise software development, and robotics hardware and software. My strengths " +
 "include: effective communication, self-motivated learning, and a relentless drive to see " +
-"things through to completion."
+"projects through to completion."
 )
 
-const myTech = ["Python/Django", "Javascript/React", "HTML/CSS"]
+const myTech = ["Python / Django", "Javascript / React", "HTML / CSS"]
 
 const myInterests = ""
 
