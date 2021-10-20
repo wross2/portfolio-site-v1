4c7d441e38c33b2c370e8fd5e67eadc2f8736314
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-20 15:38:44 -0700

    Semantics update

--- src/content/about.js
+++ src/content/about.js
@@ -1,5 +1,5 @@
 const professionalInfo = (
-"I am a software developer who holds a BSc in Electrical Engineering. " +
+"I am a software engineer who holds a BSc in Electrical Engineering. " +
 "I have worked on a range of projects including research on machine learning algorithms, " +
 "government enterprise software development, and robotics hardware and software. My strengths " +
 "include: effective communication, self-motivated learning, and a relentless drive to see " +
