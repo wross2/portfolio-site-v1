57b4a8252a04e52e46a0155bd6967802312b0a44
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-19 22:44:39 -0700

    Decreased total time for typing out greeting.

--- src/sections/CoverPage.js
+++ src/sections/CoverPage.js
@@ -4,13 +4,13 @@ import "./styles/cover-page.css";
 
 
 const typingExecution = (typewriter) => {typewriter
-    .start().pauseFor(2000).changeDelay(200)
-    .typeString("Hello.").pauseFor(1000).changeDelay(150)
+    .start().pauseFor(2000).changeDelay(50)
+    .typeString("Hello.").pauseFor(1500)
     .typeString(" My name is William Ross.").pauseFor(1000)
     .typeString(" I am a<br>Creator.").pauseFor(1000)
-    .deleteChars(8).pauseFor(500).typeString("Builder.")
+    .deleteChars(8).pauseFor(750).typeString("Builder.")
     .pauseFor(1000).deleteChars(8).pauseFor(1000)
-    .changeDelay(200).typeString("<u>Software Engineer</u>.")
+    .changeDelay(80).typeString("<u>Software Engineer</u>.")
 }
 
 class CoverPage extends Component{
@@ -29,29 +29,31 @@ class CoverPage extends Component{
     }
 
     _onScroll = (e) => {
-        const opacity = 1 - ((window.scrollY - (window.innerHeight/7)) / (window.innerHeight/2.5));
-        if (opacity > 1) {
+        const opacity = 1 - ((window.scrollY - (window.innerHeight/7)) / (window.innerHeight/3));
+        if (opacity >= 1) {
             this.setState({opacity: 1, arrowOpacity: 1, textOpacity: 1});
+            return
         }
         if (opacity < 0) {
             this.setState({opacity: 0, arrowOpacity: 0, textOpacity: 0});
+            return
         }
         
         this.setState({opacity: opacity, 
-                       arrowOpacity: opacity/4, 
-                       textOpacity: opacity/2});
+                       arrowOpacity: 0, 
+                       textOpacity: opacity/4});
     }
 
     render(){
     return (<>
     <div className="cover-sheet" style={{opacity: this.state.opacity}}></div>
-    <div className="cover-page" onScroll={this._onScroll} style={{opacity: this.state.opacity}}>
+    <div className="cover-page" onScroll={this._onScroll} style={{opacity: this.state.textOpacity}}>
         <div className="greeting-wrapper">
             <Typewriter
                 options={{
                     wrapperClassName: "Typewriter__wrapper greeting-text",
                     cursorClassName: "Typewriter__cursor greeting-cursor",
-                    deleteSpeed: 30
+                    deleteSpeed: 20
                 }}
                 onInit={typingExecution}
             />
