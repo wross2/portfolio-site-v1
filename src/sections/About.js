0fa81090ce0b3fe89b6a06cce276c4eb775892c5
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-20 02:10:43 -0700

    Added wrapping container for section (added to all sections to push with margin)

--- src/sections/About.js
+++ src/sections/About.js
@@ -10,22 +10,23 @@ class About extends Component {
         const className = this.props.className;
 
         return (
-            <section className={className} id="about-section">
+            <section className={className}>
+                <div className="section-container" id="about-section">
+                    <div className="pfp-container">
+                        <img src={myPFP} alt="William Ross" className="about-pfp"/>
+                    </div>
 
-                <div className="pfp-container">
-                    <img src={myPFP} alt="William Ross" className="about-pfp"/>
-                </div>
-
-                <div id="about-professional">
-                    {aboutData.professional}
-                </div>
-                <div id="about-tech">
-                    My web development tech stack includes:
-                    <ul id="tech-list">{
-                        aboutData.tech.map((tech, index) => {
-                            return <li key={index}>{tech}</li>
-                        })
-                    }</ul>
+                    <div className="about-professional">
+                        {aboutData.professional}
+                    </div>
+                    <div className="about-tech">
+                        My web development tech stack includes:
+                        <ul id="tech-list">{
+                            aboutData.tech.map((tech, index) => {
+                                return <li key={index}>{tech}</li>
+                            })
+                        }</ul>
+                    </div>
                 </div>
             </section>
         )
