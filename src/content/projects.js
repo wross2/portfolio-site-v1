666b5d4625f08b77b047c2d2397e9e4e65199336
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-20 15:37:05 -0700

    Semantic update

--- src/content/projects.js
+++ src/content/projects.js
@@ -5,7 +5,7 @@ const projectData = [
         codeRepo: "https://github.com/wross2/AlgoViz",
         site: "https://algov1z.netlify.app",
         tech: [
-            "HTML", "CSS", "Javascript"
+            "HTML / CSS", "Javascript"
         ],
         description: "This project is an entirely frontend based project that " +
                      "animates popular algorithms. Check out more in the Github Repo below. " +
@@ -18,7 +18,7 @@ const projectData = [
         codeRepo: "",
         site: "https://lyteway.com",
         tech: [
-            "HTML", "CSS", "Python", "Pelican CMS"
+            "HTML / CSS", "Python", "Pelican CMS"
         ],
         description: (
             "This project is a personal blog to document my investment research. " +
