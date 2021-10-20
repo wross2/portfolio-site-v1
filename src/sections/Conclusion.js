94f78115375e7f88e808e2c0ab751b97ba944322
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-20 02:12:00 -0700

    Added wrapping container for section (added to all sections to push with margin)

--- src/sections/Conclusion.js
+++ src/sections/Conclusion.js
@@ -4,8 +4,10 @@ class Conclusion extends Component {
     render() {
         const className = this.props.className;
         return (
-            <section className={className} id="conclusion-section">
-                
+            <section className={className}>
+                <div className="section-container" id="conclusion-section">
+
+                </div>
             </section>
         )
     }
