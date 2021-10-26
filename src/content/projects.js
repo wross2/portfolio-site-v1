56f01f106412f224aa9341704cbca5d0b7930ca2
williamross165 <65570419+williamross165@users.noreply.github.com>
2021-10-26 01:22:40 -0700

    Added portfolio-site-v1 to projects

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,5 +1,6 @@
 import blogSiteImg from '../images/research-blog-site.png';
-import AlgoVizImg from '../images/algoviz-site.png';
+import algoVizImg from '../images/algoviz-site.png';
+import portfolioV1Img from '../images/portfolio-site-v1.png';
 
 const projectData = [
     {
@@ -14,7 +15,7 @@ const projectData = [
                      "Check out more in the Github Repo below. " +
                      "This project is exlusively frontend and uses only " + 
                      "vanilla HTML, CSS, and Javascript.",
-        image: AlgoVizImg,
+        image: algoVizImg,
     },
     {
         id: "research-blog-project",
@@ -29,6 +30,18 @@ const projectData = [
             "It makes use of a flat file CMS called Pelican, that is built on Python."
         ),
         image: blogSiteImg,
+    },
+    {
+        id: "portfolio-site-v1",
+        name: "Portfolio Site V-1.0",
+        codeRepo: "https://github.com/wross2/portfolio-site-v1",
+        site: "https://williamross.info",
+        tech: [
+            "ReactJS", "HTML / CSS", "Javascript"
+        ],
+        description: "My personal portfolio site for showcasing myself " +
+                     "and my work. This is version 1.0, built entirely in ReactJS.",
+        image: portfolioV1Img,
     },
 ]
 
